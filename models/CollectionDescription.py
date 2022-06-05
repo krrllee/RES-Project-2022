1a55213ca468366128f5f8070e2b3aca99ad01a4
Nemanja1324 <101801298+Nemanja1324@users.noreply.github.com>
2022-06-05 17:25:20 +0200

    Inicijalizacija kolekcija i worker propertija

---
+++ models/CollectionDescription.py
@@ -0,0 +1,8 @@
+import HistoricalCollection
+
+class CollectionDescription:
+	def __init__(self,id,dataset,historical_collection):
+		self.id = id
+		self.dataset = dataset
+		self.historical_collection = historical_collection
+
---
+++ models/HistoricalCollection.py
@@ -0,0 +1,6 @@
+import WorkerProperty
+
+class HistoricalCollection:
+	def __init__(self,worker_property):
+		self.worker_property = worker_property
+
