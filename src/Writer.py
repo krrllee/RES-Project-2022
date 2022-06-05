ffeca8869783302fa8c8eff4f007d543449e8406
Dragana Banic <draganabanic00@gmail.com>
2022-06-05 23:11:06 +0200

    Izmenjen Writer

--- src/Writer.py
+++ src/Writer.py
@@ -1,5 +1,7 @@
+from http import server
 import socket
 import pickle
+from sqlite3 import connect
 import time
 import random
 
@@ -7,25 +9,53 @@ import random
 from models import Item as Item
 
 HEADERSIZE = 10
-
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),1234))
-
+LOCALHOST = socket.gethostbyname(socket.gethostname())
+PORT = 1234
 listaKodova = ["CODE_ANALOG","CODE_DIGITAL","CODE_CUSTOM","CODE_LIMITSET","CODE_SINGLENOE","CODE_MULTIPLENODE","CODE_CONSUMER","CODE_SOURCE"]
 
-def saljiPodatke():
-    while True:
-        
-        data = Item(random.choice(listaKodova),random.randint(0,200))
-        print(f"Code: {data.code} Value: {data.value}")
-        pack = pickle.dumps(data)
-        s.send(pack)
-        time.sleep(2)
+class Writer:
+    server : object
+    konektovana_adresa : object
 
+    def __init__(self, server_adresa):
+        self.server_adresa=server_adresa
+        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    def Connect(self):
+        self.sock, self.konektovana_adresa = self.sock.connect((self.server_adresa,PORT))
+
+
+    def Komande(self):
+        while(True):
+            print("\n\t1 - UPALI\n \t2 - UGASI")
+            opcija=int(input())
+            poruka=""
+            if(opcija==1):
+                poruka="Upali"
+            elif(opcija==2):
+                poruka="Ugasi"
+            else:
+                print("\t Zadata opcija ne postoji.")
+
+            if poruka!="" :
+                self.socket.connect(self.server_addres)
+                
+
+    def saljiPodatke(self):
+        while True:
+            
+            data = Item(random.choice(listaKodova),random.randint(0,200))
+            print(f"Code: {data.code} Value: {data.value}")
+            pack = pickle.dumps(data)
+            self.sock.send(pack)
+            time.sleep(2)
+
+    def PrimiPodatke(self):
+        msg = self.sock.recv(1024)
+        msg = msg.decode("utf-8")
 def main():
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
-    saljiPodatke()
+    writer1 = Writer(LOCALHOST)
+    writer1.saljiPodatke()
 
 if __name__ == "__main__":
     main()
