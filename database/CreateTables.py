74a4e567f070385cdbafa6ba5580c09bfe94a86c
DusanKne <101563317+DusanKne@users.noreply.github.com>
2022-06-19 00:29:54 +0200

    Add files via upload

--- database/CreateTables.py
+++ database/CreateTables.py
@@ -4,7 +4,7 @@ db = mysql.connector.connect(host = "127.0.0.1", user = "root", passwd = "root",
 myCursor = db.cursor()
 
 def CreateTables():
-    myCursor.execute("CREATE TABLE DataSet1(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL)")
-    myCursor.execute("CREATE TABLE DataSet2(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL)")
-    myCursor.execute("CREATE TABLE DataSet3(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL)")
-    myCursor.execute("CREATE TABLE DataSet4(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL)")
+    myCursor.execute("CREATE TABLE DataSet1(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL, date datetime)")
+    myCursor.execute("CREATE TABLE DataSet2(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL, date datetime)")
+    myCursor.execute("CREATE TABLE DataSet3(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL, date datetime)")
+    myCursor.execute("CREATE TABLE DataSet4(id int PRIMARY KEY NOT NULL AUTO_INCREMENT, code VARCHAR(50) NOT NULL, value int NOT NULL, date datetime)")
--- database/DataBaseFunction.py
+++ database/DataBaseFunction.py
@@ -3,8 +3,8 @@ import mysql.connector
 db = mysql.connector.connect(host = "127.0.0.1", user = "root", passwd = "root", database = "LoadBalancerDB")
 myCursor = db.cursor()
 
-def ubaci(value, code, dataset):
-    myCursor.execute(f"INSERT INTO {dataset} (value, code) VALUES (%s, %s)", (value, code))
+def ubaci(value, code, date, dataset):
+    myCursor.execute(f"INSERT INTO {dataset} (value, code, date) VALUES ({value}, {code}, {date})")
     db.commit()
 
 def ispisi(dataset):
